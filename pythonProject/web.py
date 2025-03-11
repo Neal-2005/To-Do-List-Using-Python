9a543a80c218eb364dbd4f1ed174377213f6fe19
Neal Mhatre <mhatreneal@gmail.com>
2025-03-11 22:25:11 +0530

    Added ability to add todos to the list

--- pythonProject/web.py
+++ pythonProject/web.py
@@ -2,17 +2,27 @@ import streamlit as st
 import functions
 
 todos  = functions.get_todo()
+def add_todo():
+    todo = st.session_state["new_todo"] + '\n' #session state is like a dictionary
+    todos.append(todo)
+    functions.write_todo(todos)
+
+
+
+
 
 st.title("My Todo-App")
 st.subheader("This is my To-Do project")
 st.write("This app is for increasing your productivity")
 
 
+
 for todo in todos:
     st.checkbox(todo)
 
 
-st.text_input(label = "", placeholder="Enter To-Do")
+st.text_input(label = "", placeholder="Enter To-Do...",
+              on_change = add_todo, key = "new_todo")
 
 
 
