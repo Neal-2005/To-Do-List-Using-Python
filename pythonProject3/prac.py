2f68802e2abdd8929a7100d58a01a17cfb0c3ba1
Neal Mhatre <63789690+Neal-2005@users.noreply.github.com>
2025-03-23 08:57:25 +0530

    Delete pythonProject3 directory

--- pythonProject3/prac.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-# Creating a DataFrame from a dictionary
-data = {'Name': ['Alice', 'Bob', 'Charlie', 'David'],
-        'Age': [25, 30, 35, 28],
-        'City': ['New York', 'San Francisco', 'Los Angeles', 'Chicago']}
-df = pd.DataFrame(data)
-print(df)
